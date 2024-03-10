979ad1539684d7c817992284b4f51604fa001687
Farid Ibrahim <faridibrahim000@gmail.com>
2024-03-10 19:13:31 +0100

    404 error page

---
+++ src/pages/404.js
@@ -0,0 +1,21 @@
+import styles from '@/styles/underDevelopment.module.css';
+import Image from 'next/image';
+
+function UnderDevelopment() {
+
+    return (
+        <div className={styles.underDevelopmentDiv}>
+            <Image
+                alt="404 error gif"
+                src="/gifs/404 Error Page.gif"
+                width={500}  
+                height={500}
+                className={styles.underDevelopmentGif}
+                priority
+            />
+        </div>
+    );
+
+}
+
+export default UnderDevelopment;
--- src/pages/[category]/index.jsx
+++
@@ -1,7 +0,0 @@
-function categoryPage() {
-    return (
-        <h1>Category Page</h1>
-    );
-}
-
-export default categoryPage;
